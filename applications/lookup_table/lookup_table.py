4bc87897a64e863fa1e5f4ab0dfd0a9fa030b110
Kelly Moreira <moreira.kd@outlook.com>
2020-06-04 16:42:35 -0400

    dryer lookup

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -17,15 +17,20 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-    if (x, y) not in cache:
+    # if (x, y) not in cache:
 
-        v = math.pow(x, y)
-        v = math.factorial(v)
-        v //= (x + y)
-        v %= 982451653
-        cache[x, y] = v
+    #     v = math.pow(x, y)
+    #     v = math.factorial(v)
+    #     v //= (x + y)
+    #     v %= 982451653
+    #     cache[x, y] = v
 
-    return cache[(x, y)]
+    # return cache[(x, y)]
+    key = (x, y)
+
+    if key not in cache:
+        cache[key] = slowfun_too_slow(x, y)
+    return cache[key]
 
 
 
